2c4c3c7add49b5580bddabbf9f8f5402ac77f318
Dugelon <47793633+Dugelon@users.noreply.github.com>
2021-04-08 16:54:36 +0200

    Update ubuntu-install.sh

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -4,5 +4,5 @@ sudo apt-get update
 sudo apt-get install ansible -y
 sudo apt-get install git -y
 sudo rm /tmp/ubuntu-install.yml*
-wget https://raw.githubusercontent.com/Dugelon/ubuntu-install-baseline/master/ubuntu-install.yml -P /tmp/
+wget https://raw.githubusercontent.com/Dugelon/workingonubuntu/main/ubuntu-install.yml -P /tmp/
 sudo ansible-playbook /tmp/ubuntu-install.yml 
